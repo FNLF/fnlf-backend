9c0a8d0e59f457a26ba83973691f3fdf13fefefe
Einar Huseby <einar.huseby@gmail.com>
2014-12-21 21:51:28 +0100

    Hotfix new domains

---
+++ apps/clubs.py
@@ -0,0 +1,52 @@
+"""
+
+    Clubs
+    =====
+    
+    Clubs and extra information
+    
+"""
+
+_schema = {
+            'id': {'type': 'string',
+                   'required': True,
+                 },
+            'name': {'type': 'string',
+                   },
+            'active': {'type': 'boolean',
+                       },
+            'org': {'type': 'string',
+                    },
+            'locations': {'type': 'dict'}, #Should be refs or embedded locations??
+            'planes': {'type': 'dict'}, #Should be refs or embedded planes??
+            'roles' : {'type': 'dict'}, #Should be refs or embedded roles
+            'ot': {'type': 'integer',
+                   'required': True,
+                   'allowed': [1,2]},
+            'ci': {'type': 'integer', 'required': False}, #Embedded or??
+            'logo': {'type': 'media', 'required': False},
+            'url': {'type': 'string', 'required': False},
+            }
+
+definition = {
+        'item_title': 'club',
+        'url': 'melwin/clubs',
+        'description': 'Melwin passthrough',
+        
+        'datasource': {'source': 'clubs',
+                       'default_sort': [('id', 1)],
+                       },
+        
+        'resource_methods': ['GET', 'POST', 'DELETE'],
+        'item_methods': ['GET', 'PATCH', 'PUT'],
+        
+        'versioning': True,
+        
+        #Make lookup on club id from melwin
+        'additional_lookup': {
+            'url': 'regex("[\d{3}\-\w{1}]+")',
+            'field': 'id',
+        },
+        
+        'schema': _schema,
+}
---
+++ apps/licenses.py
@@ -0,0 +1,51 @@
+"""
+
+    license
+    =======
+    
+    Should return an object with license information
+    
+    {
+        "_id" : ObjectId("5360ad6712afba95dd35a527"),
+        "id" : 9,
+        "melwinId" : "F-C",
+        "licenseName" : "C-lisens",
+        "active" : true
+    }
+
+"""
+
+_schema = {
+            'id': {'type': 'string',
+                   'required': True,
+                 },
+            'name': {'type': 'string',
+                   },
+            'active': {'type': 'boolean',
+                       },
+            
+            'url': {'type': 'string', 'required': False},
+            }
+
+definition = {
+        'item_title': 'licenses',
+        #'item_url': 'clubs',
+        'url': 'melwin/licenses',
+        'description': 'Melwin passthrough',
+        
+        'datasource': {'source': 'licenses',
+                       'default_sort': [('id', 1)],
+                       },
+        
+        'resource_methods': ['GET', 'POST', 'DELETE'],
+        'item_methods': ['GET', 'PATCH', 'PUT'],
+        
+        'versioning': True,
+        
+        'additional_lookup': {
+            'url': 'regex("[\w{1}\-\w{1,5}]+")',
+            'field': 'id',
+        },
+        
+        'schema': _schema,
+}
