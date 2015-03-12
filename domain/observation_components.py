dc9186c91e1734511cf05b4d2961d11b5003c057
Einar Huseby <einar.huseby@gmail.com>
2015-03-12 16:27:12 +0100

    Added sort and active to observation components

--- domain/observation_components.py
+++ domain/observation_components.py
@@ -51,6 +51,8 @@ _schema = {'what': {'type': 'string',
                           },
            
            'help': {'type': 'dict'},
+           'sort': {'type': 'integer'},
+           'active': {'type': 'boolean'},
            }
 
 definition = {
