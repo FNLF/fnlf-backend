45f2add6d35bde3d9c5289ccde4ec8039d6f8204
Einar Huseby <einar.huseby@gmail.com>
2015-03-15 18:35:45 +0100

    Hotfix checks for organization

--- run.py
+++ run.py
@@ -275,22 +275,26 @@ def __anonymize_obs(item):
         
         if 'hl' in item['organization']:    
             for k, hl in enumerate(item['organization']['hl']):
-                item['organization']['hl'][k]['id'] = -1
+                if 'id' in item['organization']['hl'][k]:
+                    item['organization']['hl'][k]['id'] = -1
                 if 'tmpname' in  item['organization']['hl'][k]:
                     del item['organization']['hl'][k]['tmpname']
         if 'hfl' in item['organization']:          
             for k, hfl in enumerate(item['organization']['hfl']):
-                item['organization']['hfl'][k]['id'] = -1
+                if 'id' in item['organization']['hfl'][k]:
+                    item['organization']['hfl'][k]['id'] = -1
                 if 'tmpname' in  item['organization']['hfl'][k]:
                     del item['organization']['hfl'][k]['tmpname']
         if 'hm' in item['organization']:          
             for k, hm in enumerate(item['organization']['hm']):
-                item['organization']['hm'][k]['id'] = -1
+                if 'id' in item['organization']['hm'][k]:
+                    item['organization']['hm'][k]['id'] = -1
                 if 'tmpname' in item['organization']['hm'][k]:
                     del item['organization']['hm'][k]['tmpname']
         if 'pilot' in item['organization']:          
             for k, pilot in enumerate(item['organization']['pilot']):
-                item['organization']['pilot'][k]['id'] = -1
+                if 'id' in item['organization']['pilot'][k]:
+                    item['organization']['pilot'][k]['id'] = -1
                 if 'tmpname' in item['organization']['pilot'][k]:
                     del item['organization']['pilot'][k]['tmpname']
     
