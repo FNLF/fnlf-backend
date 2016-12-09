cafbb54a0de0d87a4ceda1f4a733264a7462c575
Einar Huseby <einar.huseby@gmail.com>
2016-12-09 17:49:42 +0100

    Domain cleanup and code formatting

--- domain/observations.py
+++ domain/observations.py
@@ -12,101 +12,102 @@
     @todo: add schema for organisation or club + location
 """
 from _base import workflow_schema, comments_schema, watchers_schema, audit_schema, acl_item_schema, ask_schema
-import observation_components
+from observation_components import components_schema
 
 _schema = {'id': {'type': 'integer',
                   'required': False,
-                  'readonly': True},
-           
+                  'readonly': True
+                  },
+
            'type': {'type': 'string',
-                    'allowed': ['sharing', 'unsafe_act', 'near_miss', 'incident', 'accident']},
-           
+                    'allowed': ['sharing', 'unsafe_act', 'near_miss', 'incident', 'accident']
+                    },
+
            'flags': {'type': 'dict',
                      'schema': {'aviation': {'type': 'boolean', 'default': False},
-                                'insurance': {'type': 'boolean', 'default': False},
+                                'insurance': {'type': 'boolean', 'default': False}
                                 }
-                    },
+                     },
            'ask': ask_schema,
-           
+
            'tags': {'type': 'list',
-                    'default': []},
-           
+                    'default': []
+                    },
+
            'club': {'type': 'string',
-                    'required': True},
-           
+                    'required': True
+                    },
+
            'location': {'type': 'dict'},
-           
-           'owner': {'type': 'integer', 'readonly': True}, # user_id this post/patch
-           'reporter': {'type': 'integer', 'readonly': True}, # user_id initial reported by!
-           
+
+           'owner': {'type': 'integer', 'readonly': True},
+           'reporter': {'type': 'integer', 'readonly': True},
+
            'when': {'type': 'datetime'},
-           
+
            'involved': {'type': 'list',
-                        'default': []},
-           
+                        'default': []
+                        },
+
            'organization': {'type': 'dict',
                             'default': {}
                             },
-           
+
            'rating': {'type': 'dict',
                       'schema': {'actual': {'type': 'integer'},
-                                 'potential': {'type': 'integer'},
-                                 'user': {'type': 'float', 
-                                          'readonly': True},
+                                 'potential': {'type': 'integer'}
                                  }
                       },
            'weather': {'type': 'dict',
                        'schema': {'auto': {'type': 'dict'},
-                                  'manual': {'type': 'dict'},
+                                  'manual': {'type': 'dict'}
                                   }
                        },
-           
-           'components': {'type': 'list',
-                          'schema': observation_components._schema,
-                          #'schema': {''} # Possibly the same as components
+
+           'components': {'type': 'list'
+
                           },
-           
-           'files': {'type': 'list', 
-                     'schema': {'type': 'dict', 
-                                'schema': {'f': {'type': 'string'}, 
-                                           'r': {'type': 'boolean'}}
-                                }, #f: objectid
-                     'default': [],
+
+           'files': {'type': 'list',
+                     'schema': {'type': 'dict',
+                                'schema': {'f': {'type': 'string'},
+                                           'r': {'type': 'boolean'}
+                                           }
+                                },
+                     'default': []
                      },
-           
-           'related': {'type': 'list', 
-                       'default': []},
+
+           'related': {'type': 'list',
+                       'default': []
+                       },
            'actions': {'type': 'dict'},
-           
            'comments': comments_schema,
            'workflow': workflow_schema,
            'watchers': watchers_schema,
            'audit': audit_schema,
-           'acl': acl_item_schema,
-           
-           
-           }
+           'acl': acl_item_schema
 
+           }
+# 'schema': components_schema
 definition = {
-        'item_title': 'observations',
-        'datasource': {'source': 'observations',
-                       'projection': {'acl': 0} #'files': 0,  
-                       },
-        
-        # Make a counter so we can have a lookup for #455
-        'additional_lookup': {
-            'url': 'regex("[\d{1,9}]+")',
-            'field': 'id',
-        },
-        'extra_response_fields': ['id'],
-        #makes only user access those...
-        #'auth_field': 'owner',
-        
-        'versioning': True,
-        
-        'resource_methods': ['GET', 'POST'],
-        'item_methods': ['GET', 'PATCH', 'PUT'],
-        
-        'schema': _schema
-        
-       }
+    'item_title': 'observations',
+    'datasource': {'source': 'observations',
+                   'projection': {'acl': 0}  # 'files': 0,
+                   },
+    # Make a counter so we can have a lookup for #455
+    'additional_lookup': {
+        'url': 'regex("[\d{1,9}]+")',
+        'field': 'id',
+    },
+    'extra_response_fields': ['id'],
+    # makes only user access those...
+    # 'auth_field': 'owner',
+
+    'versioning': True,
+
+    'resource_methods': ['GET', 'POST'],
+    'item_methods': ['GET', 'PATCH', 'PUT'],
+
+    'schema': _schema
+
+}
