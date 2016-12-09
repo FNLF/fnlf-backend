27b0f5230b3392d0284374623c822e5a396de138
Einar Huseby <einar.huseby@gmail.com>
2016-12-09 17:47:38 +0100

    Fixes #93

--- ext/weather/aeromet.py
+++ ext/weather/aeromet.py
@@ -22,21 +22,21 @@ class Aeromet():
 
     def metar(self):
         metar = ""
-        for line in urlopen('http://weather.noaa.gov/pub/data/observations/metar/stations/%s.TXT' % self.airport):
+        for line in urlopen('http://tgftp.nws.noaa.gov/data/observations/metar/stations/%s.TXT' % self.airport):
             metar += "%s " % line.decode("utf-8").strip()
                 
         return metar.strip()
 
     def taf(self):
         taf = ""
-        for line in urlopen('http://weather.noaa.gov/pub/data/forecasts/taf/stations/%s.TXT' % self.airport):
+        for line in urlopen('http://tgftp.nws.noaa.gov/pub/data/forecasts/taf/stations/%s.TXT' % self.airport):
             taf += "%s " % line.decode("utf-8").strip()
                 
         return taf.strip()
     
     def shorttaf(self):
         shorttaf = ""
-        for line in urlopen('http://weather.noaa.gov/pub/data/forecasts/shorttaf/stations/%s.TXT' % self.airport):
+        for line in urlopen('http://tgftp.nws.noaa.gov/pub/data/forecasts/shorttaf/stations/%s.TXT' % self.airport):
             shorttaf += "%s " % line.decode("utf-8").strip()
                 
         return shorttaf.strip()
