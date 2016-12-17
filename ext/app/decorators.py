12e044f09d7d0c14be7d2da60f482897fe36d680
Einar Huseby <einar.huseby@gmail.com>
2016-12-17 19:22:23 +0100

    Added require_superadmin decorator

--- ext/app/decorators.py
+++ ext/app/decorators.py
@@ -9,6 +9,7 @@ import arrow
 from datetime import datetime
 
 from ext.auth.tokenauth import TokenAuth
+from ext.auth.helpers import Helpers
 
 from threading import Thread
 
@@ -57,4 +58,28 @@ def require_token():
                 
             return f(*args, **kwargs)
         return wrapped
-    return decorator 
+
+    return decorator
+
+
+def require_superadmin():
+    """Require user to be in a group of hardcoded user id's
+    Should use Helpers then get administrators
+    @TODO: use a switch for ref [superadmin, admin,..]?
+    @TODO: in ext.auth.helpers define a get_users_in_roles_by_ref(ref)?
+    """
+
+    def decorator(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            h = Helpers()
+
+            if int(app.globals['user_id']) not in h.get_superadmins():
+                resp = Response(None, 401)
+                abort(401, description='Please provide proper credentials', response=resp)
+
+            return f(*args, **kwargs)
+
+        return wrapped
+
+    return decorator
