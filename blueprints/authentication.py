01bd5e49b9e4b92d55ff465f71348348e2848815
Einar Huseby <einar.huseby@gmail.com>
2018-12-22 12:45:27 +0100

    Corrected public cert name

--- blueprints/authentication.py
+++ blueprints/authentication.py
@@ -33,7 +33,7 @@ import os.path
 
 def _get_public_key():
     public_key = None
-    with open('ors-public.pem', 'rb') as f:
+    with open('app-public.pem', 'rb') as f:
         public_key = f.read()
     return public_key
 
